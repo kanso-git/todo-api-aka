b12624cd58bb56a73eecd72393fde9bbcb867875
Abdallah Kanso <abdallah.kanso@gmail.com>
2016-04-26 22:47:14 +0200

    add put method

--- server.js
+++ server.js
@@ -64,11 +64,51 @@ app.delete('/todos/:id', function(req, res) {
         res.json(matchedTodo);
     } else {
         res.status(404).send({
-            "erroe": "no todo found with id[" + idToFind + "]"
+            "error": "no todo found to delete with id[" + idToFind + "]"
         });
     }
 
 })
+
+// PUT /todos/id
+
+app.put('/todos/:id', function(req, res) {
+    var validAttributes = {};
+    var idToFind = parseInt(req.params.id);
+    var body = _.pick(req.body, 'description', 'completed');
+
+    var matchedTodo = _.findWhere(todos, {
+        id: idToFind
+    });
+    if (!_.isObject(matchedTodo)) {
+        return res.status(404).send({
+            "error": "no todo found to update with id[" + idToFind + "]"
+        });
+    }
+
+    if (body.hasOwnProperty('completed')) {
+        if (_.isBoolean(body.completed)) {
+            validAttributes.completed = body.completed;
+        } else {
+            return res.status(400).send();
+        }
+    }
+
+    if (body.hasOwnProperty('description')) {
+        if (_.isString(body.description) && body.description.trim().length > 0) {
+            validAttributes.description = body.description;
+        } else {
+            return res.status(400).send();
+        }
+    }
+
+    if (!_.isEmpty(validAttributes)) {
+        //perform the update  arrays are passed by reference even in Javascript
+        _.extend(matchedTodo, validAttributes);
+    }
+    res.json(matchedTodo);
+
+});
 
 app.listen(PORT, function() {
     console.log("todo-api  server is runing on port:" + PORT);
