008a45b0f1133e9ad660dd4b780472d2a300d7f8
Abdallah Kanso <abdallah.kanso@gmail.com>
2016-04-26 19:53:04 +0200

    Add parser-body

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-        id: 1,
-        description: 'Meet mom for lunch',
-        completed: false
-    }, {
-        id: 2,
-        description: 'Go to market',
-        completed: false
-    }, {
-        id: 3,
-        description: 'Feed the cat',
-        completed: true
-    }
+var todos = [];
+var todoNextId = 1;
 
-];
+// parse application/json
+app.use(bodyParser.json())
 
 
 app.get('/', function(req, res) {
@@ -40,6 +32,15 @@ app.get('/todos/:id', function(req, res) {
 
 })
 
+
+// POST todos
+app.post('/todos', function(req,res){
+    var body = req.body;
+    body.id=todoNextId;    
+    todos.push(body);
+    todoNextId = todoNextId +1;
+    res.json(body);
+});
 
 app.listen(PORT, function() {
     console.log("todo-api  server is runing on port:" + PORT);
